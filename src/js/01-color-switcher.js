f8f4767ab267da9f7e10b0bce191b2d6951fbdcd
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2022-08-10 20:22:56 +0300

    add task_01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,40 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+refs.btnStart.addEventListener('click', startRenderRandomColor);
+refs.btnStop.addEventListener('click', stopRenderRandomColor);
+
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+// Запускаем изменения цвета по клику
+// Кнопка старт не активна
+// Устанавливаем интервал
+function startRenderRandomColor(event) {
+  refs.btnStart.disabled = true;
+
+  timerId = setInterval(() => {
+    getRandomHexColor();
+    setBackroundColor();
+  }, 1000);
+}
+
+// Задаём цвет на body
+function setBackroundColor() {
+  const randomColor = getRandomHexColor();
+  refs.body.style.backgroundColor = randomColor;
+}
+
+// Останавливаем изменения цвета при клике на кнопку Stop
+// Кнопка старт активна
+// Удаляем интервал
+function stopRenderRandomColor(event) {
+  refs.btnStart.disabled = false;
+  clearInterval(timerId);
+}
